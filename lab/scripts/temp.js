ce64d1cec76080baf3600c896c9658f11a9f4879
Siddharth <sidp2906@gmail.com>
2024-10-15 16:12:47 +0100

    Head to body changed

--- lab/scripts/temp.js
+++ lab/scripts/temp.js
@@ -1,6 +1,6 @@
 function D3draw(){
     let dataset = [5,10,15,20,25,30,35,40,50,60];
-    d3.select("head").append("H1").text("New Heading!")
+    d3.select("body").append("H1").text("New Heading!")
     d3.select("body").append("p").text("Check")
 
     d3.select("body").selectAll("p")
