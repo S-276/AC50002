94a29e1694eef204f9c9117311fe35da55054530
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-20 16:49:41 +0100

    Update ex4.js

--- lab/scripts/ex4.js
+++ lab/scripts/ex4.js
@@ -1,14 +1,14 @@
-function D3Draw(){
+function d3Draw(){
 let dataset = [5,10,15,20,25,30];
   
-d3.select("body").selectAll("p")
- .data(dataset)
- .enter()
- .append("p")
- .text(Bob);
+  d3.select("body").selectAll("p")
+   .data(dataset)
+   .enter()
+   .append("p")
+   .text(Bob);
 }
 window.onload = D3Draw;
 
 function Bob(data){
-return "new paragraph" + data;
+  return "new paragraph" + data;
 }
