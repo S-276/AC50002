9b3dd8d619c0ee71631d2cb838e3019a75b94379
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-20 16:44:39 +0100

    Create ex4.js

---
+++ lab/scripts/ex4.js
@@ -0,0 +1,14 @@
+function D3Draw(){
+let dataset = [5,10,15,20,25,30];
+  
+d3.select("body").selectAll("p")
+ .data(dataset)
+ .enter()
+ .append("p")
+ .text(Bob);
+}
+window.onload = D3Draw;
+
+function Bob(data){
+return "new paragraph" + data;
+}
