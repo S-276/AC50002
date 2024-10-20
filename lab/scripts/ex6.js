9d6b0ba8e3d085e426251c152c6bab8ab4f01c98
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-20 17:22:47 +0100

    Added .data(dataset)

--- lab/scripts/ex6.js
+++ lab/scripts/ex6.js
@@ -1,7 +1,7 @@
 function d3Draw(){
   let dataset = [5,10,15,20,25,30,60,70];
 
-  d3.select("body").selectAll("div").enter().append("div").attr("class","bar").style("height",function(d) {
+  d3.select("body").selectAll("div").data(dataset).enter().append("div").attr("class","bar").style("height",function(d) {
     return 2 * d + px;
   }).style("background-color",function(d){
     return "#" + 2*d+2*d+2*d;
