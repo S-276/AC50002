a4f8e380d221102c41ee29e9ad5aa35163d30b87
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-21 10:43:33 +0100

    Create ex11.js

---
+++ lab/scripts/ex11.js
@@ -0,0 +1,24 @@
+function d3Draw(dataset){
+  Width = 600;
+  Height = 400;
+
+  let svg = d3.select("body").append("svg").attr("width",Width).attr("height",Height);
+  let circles = svg.selectAll("circle").data(dataset).enter().append("circle");
+  circles.attr("cx",function(d){
+    return d.x;
+  }).attr("cy",function(d){
+    return d.y;
+  }).attr("r",5);
+}
+
+function loadData(){
+  d3.csv("http://34.147.162.172/Circles/Circles/50",function(error,data){
+    if(error){
+      console.log(error)
+    }else{
+      d3Draw(data);
+  }
+  };
+         }
+
+window.onload=loadData;
