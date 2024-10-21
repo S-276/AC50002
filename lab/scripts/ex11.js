bec6dd8ccdac8399b727d6dca056621501a1bb25
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-21 11:23:26 +0100

    Update ex11.js

--- lab/scripts/ex11.js
+++ lab/scripts/ex11.js
@@ -4,17 +4,17 @@ function d3Draw(dataset){
 
   let svg = d3.select("body").append("svg").attr("width",Width).attr("height",Height);
   let circles = svg.selectAll("circle").data(dataset).enter().append("circle");
-  circles.attr("cx",function(d){
+  circles.attr("r",function(d){
+    return d.r;
+  }).attr("cx",function(d){
     return d.x;
   }).attr("cy",function(d){
     return d.y;
-  }).attr("r",function(d){
-    return d.r;
   });
 }
 
 function loadData() {
-  d3.json("http://34.147.162.172/Circles/Circles/50", function(error, data) {
+  d3.json("https://34.147.162.172/Circles/Circles/50", function(error, data) {
     if (error) {
       console.log("Error:", error);
       console.log("Status code:", error.target.status);  // Log status code
