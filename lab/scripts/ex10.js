703ec9cbae9d7b85f87e867144174cfd1dbc5282
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-21 10:23:38 +0100

    Create ex10.js

---
+++ lab/scripts/ex10.js
@@ -0,0 +1,20 @@
+function d3Draw(dataset){
+  Width = 600;
+  Height = 400;
+
+  let svg = d3.select("body").append("svg").attr("width",Width).attr("height",Height);
+  let circles = svg.selectAll("circle").data(dataset).enter().append("circle");
+  circles.attr("cx",function(d){
+    return d.x;
+  }).attr("cy",function(d){
+    return d.y;
+  }).attr("r",5);
+}
+
+function loadData(){
+  d3.csv("data.txt",function(data){
+    d3Draw(data);
+  });
+}
+
+window.onload=loadData;
