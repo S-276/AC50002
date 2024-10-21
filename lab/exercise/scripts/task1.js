a6df78dbed7204c4c11b8c5889fa8230678a2d2d
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-21 12:08:52 +0100

    Update task1.js

--- lab/exercise/scripts/task1.js
+++ lab/exercise/scripts/task1.js
@@ -1,6 +1,6 @@
 function ChangeHTML(){
   let oldParagraph=document.getElementById('Part1');
-  let newPararaph = "";
+  let newParagraph = "";
 
   for(let i = 0;i<10;i++){
     newParagraph += "This is a new paragraph, Paragraph" + ""+ i;
