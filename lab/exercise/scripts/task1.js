1de10bba7668536d9304f6de78caf2a4705d9ada
S-276 <134470512+S-276@users.noreply.github.com>
2024-10-21 12:50:27 +0100

    Update task1.js

--- lab/exercise/scripts/task1.js
+++ lab/exercise/scripts/task1.js
@@ -4,6 +4,8 @@ function ChangeHTML(){
 
   for(let i = 0;i<10;i++){
     newParagraph += "This is a new paragraph, Paragraph" + ""+ i;
+    let paragraphElement = document.createElement('p');
+    paragraphElement.innerHTML = newParagraph;
+    oldParagraph.appendChild(paragraphElement);
   }
-  oldParagraph.innerHTML=newParagraph;
 }
