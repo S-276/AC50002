f2a2ad49d9b44341d14b6b6ee3002f22717a8537
Siddharth <sidp2906@gmail.com>
2024-10-22 14:27:34 +0100

    Corrected the URL

--- lab/scripts/ex11.js
+++ lab/scripts/ex11.js
@@ -14,7 +14,7 @@ function d3Draw(dataset){
 }
 
 function loadData() {
-  d3.json("https://34.147.162.172/Circles/Circles/50", function(error, data) {
+  d3.json("http://34.147.162.172/Circles/Circles/50", function(error, data) {
     if (error) {
       console.log("Error:", error);
       console.log("Status code:", error.target.status);  // Log status code
