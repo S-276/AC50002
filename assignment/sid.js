e41e040d1a06f31af7230073372a15375ffc4035
Siddharth <sidp2906@gmail.com>
2024-11-07 16:36:23 +0000

    Syntax errors

--- assignment/sid.js
+++ assignment/sid.js
@@ -1,91 +1,122 @@
-function initmap(){
-  const map = L.map('map').setView([54.505,-2.09],6); //Latitiude,Longitude,Zoom Level
-  //Loading OpenStreetMap from Leaflet Map
-  l.titlelayer('https://{s}.title.openstreetmap.org/{x}{y}{z}/png',{attribution:'OpenStreetMap contributors'
-                                                                   }).addTo(map);
+// Initialize the Leaflet map
+function initMap() {
+  const map = L.map('map').setView([54.505, -2.09], 6); // Latitude, Longitude, Zoom Level
+
+  // Loading OpenStreetMap tiles for Leaflet map
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+
   return map;
 }
-function TownMarker(town,map,tootltip){
-  const LatLong = new L.LatLong(town.lat,town.lng);
-  const marker = L.circleMarker(LatLong,(
-    radius:5,
-    color:'red',
-    fillColor:'#f03',
-    fillOpacity:0.6
-    }).addTo(map);
-    marker.on("mouseover",function(event){
-      tooltip.html('<strong>Town:</strong> ${town.Town}<br> <strong>Population:</strong> ${town.Population}<br> <strong>Latitude:</strong> ${town.lat}<br> <strong>Longitude:</strong> ${town.lng}')
-      .style("visibility","visible")
-      .style("left",(event.originalEvent.pageX + 10) + "px")
-      .stlye("top",(event.originalEvent.pageY - 10) + "px");
-    })
-    .on("mouseout",function(){
-      tooltip.style("visibility","hidden");
-    });
-  }
-function TownData(map){
-  const tooltip = d3.select(".tooltip");
-  d3.json("http://34.147.162.172/Circles/Towns/50").then(function(towns){
-    towns.forEach(town => {
-      TownMarker(town,map,tooltip);
-    });
+
+// Function to create a marker for each town
+function TownMarker(town, map, tooltip) {
+  const latLng = new L.LatLng(town.lat, town.lng);
+
+  // Create a circle marker
+  const marker = L.circleMarker(latLng, {
+      radius: 5,
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.6
+  }).addTo(map);
+
+  // Show tooltip on hover
+  marker.on("mouseover", function(event) {
+      tooltip.html(`<strong>Town:</strong> ${town.Town}<br> 
+                    <strong>Population:</strong> ${town.Population}<br> 
+                    <strong>Latitude:</strong> ${town.lat}<br> 
+                    <strong>Longitude:</strong> ${town.lng}`)
+             .style("visibility", "visible")
+             .style("left", (event.originalEvent.pageX + 10) + "px")
+             .style("top", (event.originalEvent.pageY - 10) + "px");
+  })
+  .on("mouseout", function() {
+      tooltip.style("visibility", "hidden");
   });
 }
-function initSVGOverlay(map){
+
+// Function to fetch town data and create markers
+function TownData(map) {
+  const tooltip = d3.select(".tooltip");
+
+  d3.json("http://34.147.162.172/Circles/Towns/50").then(function(towns) {
+      towns.forEach(town => {
+          TownMarker(town, map, tooltip);
+      });
+  });
+}
+
+// Initialize an SVG overlay on the map
+function initSVGOverlay(map) {
   const svg = d3.select(map.getPanes().overlayPane).append("svg")
-  .attr("width", map.getSize().x)
-  .attr("height",map.getSize().y);
-  const g = svg.append("g").attr("class","leaflet-zoom-hide");
+                .attr("width", map.getSize().x)
+                .attr("height", map.getSize().y);
+
+  const g = svg.append("g").attr("class", "leaflet-zoom-hide");
   return g;
 }
-function createSVGMarker(town,g,tooltip,map){
-  const LatLong = map.LatLongToLayerPoint([town.lat,town.lng]);
-  const marker = g.append("circle")
-  .attr("cx",LatLong.x)
-  .attr("cy",LatLong.y)
-  .attr("r",5)
-  .attr("fill","#f03")
-  .attr("stroke","red")
-  .attr("stroke-width",1);
 
-  marker.on("mouseover",function(event){
-    tooltip.html('<strong>Town:</strong> ${town.Town}<br> <strong>Population:</strong> ${town.Population}<br> <strong>Latitude:</strong> ${town.lat}<br> <strong>Longitude:</strong> ${town.lng}')
-      .style("visibility","visible")
-      .style("left",(event.originalEvent.pageX + 10) + "px")
-      .stlye("top",(event.originalEvent.pageY - 10) + "px");
-    })
-    .on("mouseout",function(){
-      tooltip.style("visibility","hidden");
-    });
-  }
-function updateSVGMarker(g,map,town,tooltip){
+// Function to create an SVG marker for each town
+function createSVGMarker(town, g, tooltip, map) {
+  const latLng = map.latLngToLayerPoint([town.lat, town.lng]);
+
+  const marker = g.append("circle")
+                  .attr("cx", latLng.x)
+                  .attr("cy", latLng.y)
+                  .attr("r", 5)
+                  .attr("fill", "#f03")
+                  .attr("stroke", "red")
+                  .attr("stroke-width", 1);
+
+  marker.on("mouseover", function(event) {
+      tooltip.html(`<strong>Town:</strong> ${town.Town}<br> 
+                    <strong>Population:</strong> ${town.Population}<br> 
+                    <strong>Latitude:</strong> ${town.lat}<br> 
+                    <strong>Longitude:</strong> ${town.lng}`)
+             .style("visibility", "visible")
+             .style("left", (event.pageX + 10) + "px")
+             .style("top", (event.pageY - 10) + "px");
+  })
+  .on("mouseout", function() {
+      tooltip.style("visibility", "hidden");
+  });
+}
+
+// Function to update SVG markers on zoom or pan
+function updateSVGMarkers(g, map, towns, tooltip) {
   g.selectAll("circle").remove();
   towns.forEach(town => {
-    createSVGMarker(town,g,tooltip,map);
-    });
+      createSVGMarker(town, g, tooltip, map);
+  });
 }
-function main(){
-  const map = initMap();
-  const tooltip = d3.select(."tooltip");
 
+// Main function to initialize everything
+function main() {
+  const map = initMap();
+  const tooltip = d3.select(".tooltip");
+
+  // Fetch and display town data as markers
   TownData(map);
 
+  // Add SVG overlay for town markers
   const g = initSVGOverlay(map);
-d3.json("http://34.147.162.172/Circles/Towns/50").then(function(towns) {
-        // Add SVG markers to the map
-        towns.forEach(town => {
-            createSVGMarker(town, g, tooltip, map);
-        });
 
-        // Update SVG markers on map zoom or pan
-        map.on("zoomend", function() {
-            updateSVGMarkers(g, map, towns, tooltip);
-        });
+  d3.json("http://34.147.162.172/Circles/Towns/50").then(function(towns) {
+      towns.forEach(town => {
+          createSVGMarker(town, g, tooltip, map);
+      });
 
-        map.on("moveend", function() {
-            updateSVGMarkers(g, map, towns, tooltip);
-        });
-    });
+      // Update markers on zoom or pan
+      map.on("zoomend", function() {
+          updateSVGMarkers(g, map, towns, tooltip);
+      });
+
+      map.on("moveend", function() {
+          updateSVGMarkers(g, map, towns, tooltip);
+      });
+  });
 }
 
 // Call the main function to initialize everything
