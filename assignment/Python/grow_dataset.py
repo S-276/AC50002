4caa62fa7b888b7946463874322188b3a5a7e273
Siddharth <sidp2906@gmail.com>
2024-12-13 17:56:25 +0000

    Added code for plting the cleaned data

--- assignment/Python/grow_dataset.py
+++ assignment/Python/grow_dataset.py
@@ -25,3 +25,33 @@ filtered_data = data[
 
 #Display the cleaned dataset
 print(filtered_data.head())
+
+#Step 3 : Plot the Cleaned Data
+plt.figure(figsize=(10,10))
+img = plt.imread("map7.png")
+
+#Define the Map bounds 
+map_bounds = {
+    "left": bounding_box["Longitude Min"],
+    "right": bounding_box["Longitude Max"],
+    "bottom": bounding_box["Latitude Min"],
+    "top": bounding_box["Latitude Max"]
+}
+
+#Display the map
+plt.imshow(img, extent=[map_bounds["left"],map_bounds["right"],map_bounds["bottom"],map_bounds["top"]])
+
+#Scatter Plot of sensor locations
+plt.scatter(
+    filtered_data["Longitude"],
+    filtered_data["Latitude"],
+    c = 'red',
+    s = 10,
+    label = "Sensor Locations"
+)
+
+plt.title("Sensor Locations on UK map")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.legend()
+plt.show()
