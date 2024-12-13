9b90ab2b563219b8981509523af284d42b55ff98
Siddharth <sidp2906@gmail.com>
2024-12-13 17:09:56 +0000

    Load and check the dataset

--- assignment/Python/grow_dataset.py
+++ assignment/Python/grow_dataset.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+#Step 1: Read the dataset
+file_path = "GrowLocations.csv"
+df = pd.read_csv(file_path)
+
+#Display the first few rows of the dataset
+print(df.head())
