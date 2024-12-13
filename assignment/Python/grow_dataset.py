9fb173e2af8c86a6a5fc2cb2b43d517642ebd419
Siddharth <sidp2906@gmail.com>
2024-12-13 17:15:19 +0000

    Filtering the Data

--- assignment/Python/grow_dataset.py
+++ assignment/Python/grow_dataset.py
@@ -2,7 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #Step 1: Read the dataset
 file_path = "GrowLocations.csv"
-df = pd.read_csv(file_path)
+data = pd.read_csv(file_path)
 
 #Display the first few rows of the dataset
-print(df.head())
+print(data.head())
+
+#Step 2: Filter the data to remove the invalid values
+bounding_box= {
+    "Longitude Min": -10.592,
+    "Longitude Max": 1.6848,
+    "Latitude Min": 50.681,
+    "Latitude Max": 57.985
+    }
+
+#Filtering Data
+filtered_data = data[
+    (data["Longitude"] >= bounding_box["Longitude Min"]) &
+    (data["Longitude"] <= bounding_box["Longitude Max"]) &
+    (data["Latitude"] >= bounding_box["Latitude Min"]) &
+    (data["Latitude"] <= bounding_box["Latitude Max"])
+]
+
+#Display the cleaned dataset
+print(filtered_data.head())
